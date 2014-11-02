0bd37f12028fa40cbeceac69d6f720270c8fe6a6
samuelzv <samuelzv@gmail.com>
2014-11-02 15:13:01 -0600

    Agrego la funcion seedDatabase a index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var Database = require('./lib/db');
+var SeedData = require('./lib/seedData');
 var util = require('util');
 var events = require('events');
 
@@ -6,14 +7,42 @@ var Membership = function (dbName) {
     var self = this;
     events.EventEmitter.call(self);
 
-    var db = new Database(dbName);
+    var database = new Database(dbName);
 
     self.sayHello = function () {
         console.log('Hola mundo 2');
     };
 
+    // Si las colecciones no existen o estan vacias
+    // las llena con los valores iniciales
+    self.seedDatabase = function () {
+        database.getDatabase(function (err, db) {
+            if(err) {
+                console.log('Error al iniciar los valores iniciales de la base:' + err);
+            }
+            else {
+                // checamos si hay datos
+                db.users.count(function (err, count) {
+                    if(err) {
+                        console.log('Error al obtener el numero de registros en usuarios');
+                    }
+                    else {
+                        if(count === 0) {
+                            SeedData.initialUsers.forEach(function (item) {
+                                console.log(item);
+                            });
+                        }
+                    }
+
+                });
+            }
+
+        });
+    };
+
+
     return self;
 };
 
 util.inherits(Membership, events.EventEmitter);
-module.exports = Membership
+module.exports = Membership;
